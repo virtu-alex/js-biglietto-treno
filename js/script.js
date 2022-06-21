bb40bf1a32a3e18b45662e389d64cc382d121ef5
virtu-alex <alessio.cordari@gmail.com>
2022-06-21 21:32:49 +0200

    Scusate avevo dimenticato di fare l'ultimo push dopo il ticket

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 
+
+
+// NON FA PARTE DELL'ESERCIZIO //
+const firstName = prompt("Qual'e' il tuo nome?", 'Alessio')
+const nome = document.getElementById('nome-utente');
+nome.innerHTML = firstName
+
+const lastName = prompt("Qual'e' il tuo cognome?", 'Cordari')
+const cognome = document.getElementById('cognome-utente');
+cognome.innerHTML = lastName
+
+const region_a = prompt("Da dove parti? ", 'Lazio')
+const partenza = document.getElementById('region-start');
+partenza.innerHTML = region_a
+
+const region_b = prompt('Dove vuoi arrivare?', "Lombardia")
+const arrivo = document.getElementById('region-arrival');
+arrivo.innerHTML = region_b
+
+
 /* TRACCIA:
 Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
@@ -7,24 +27,6 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va stampato in pagina forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
 */
-
-
-
-// const firstName = prompt("Qual'e' il tuo nome?", 'Teacher')
-// const nome = document.getElementById('nome-utente');
-// nome.innerHTML = firstName
-
-// const lastName = prompt("Qual'e' il tuo cognome?", 'Boolean')
-// const cognome = document.getElementById('cognome-utente');
-// cognome.innerHTML = lastName
-
-// const region_a = prompt("Da dove parti? ", 'Lazio')
-// const partenza = document.getElementById('region-start');
-// partenza.innerHTML = region_a
-
-// const region_b = prompt('Dove vuoi arrivare?', "Lombardia")
-// const arrivo = document.getElementById('region-arrival');
-// arrivo.innerHTML = region_b
 
 /*
 1- RECUPERO ELEMENTO NEL MIO FOGLIO HTML
@@ -39,25 +41,51 @@ L'output del prezzo finale va stampato in pagina forma umana (con massimo due de
 // PRIMO PROMPT, KM DA PERCORRERE
 
 const kilometers = parseInt(prompt('Quanti kilometri devi percorrere?', "379"));
-console.log('I kilometri da percorrere sono ' + kilometers);
+console.log(`I kilometri da percorrere sono ${kilometers}`);
 
-let prezzo = kilometers * 0.21
 
 // SECONDO PROMPT, ETA'
 
 const userAge = parseInt(prompt('Quanti anni hai?'))
-console.log("l'eta' che hai scelto e' " + userAge);
+console.log(`l'eta' che hai scelto e' ${userAge}`);
 
-//PRIMA CONDIZIONE SCONTO MINORENNI
+//PRIMA CONDIZIONE SCONTO
+let prezzo = kilometers * 0.21
 
 if (userAge < 18) {
     prezzo = (prezzo - prezzo * 0.2).toFixed(2);
-    console.log('lo sconto applicato e ' + prezzo);
+    console.log(`lo sconto applicato e' ${prezzo}`);
 } else if (userAge >= 65) {
     prezzo = (prezzo - prezzo * 0.4).toFixed(2);
+    console.log(`lo sconto applicato e' ${prezzo}`);
 }
 
 //STAMPO IN PAGINA
 
 const discountTicket = document.getElementById('difference');
 discountTicket.innerHTML = prezzo;
+
+const scontoMinori = prezzo * 0.2;
+const scontoAnziani = prezzo * 0.4;
+let discount;
+
+if (userAge < 18) {
+    discount = scontoMinori.toFixed(2);
+
+} else if (userAge > 65) {
+    discount = scontoAnziani.toFixed(2);
+}
+
+
+// VALIDATOR + ALERT NaN
+if (!isNaN(kilometers) && !isNaN(userAge)) {
+
+} else {
+    alert("Puoi inserire solo numeri interi e che siano maggiori di zero")
+    console.log(alert)
+}
+
+
+const sconto1 = document.getElementById('conto-scontato-minor');
+sconto1.innerHTML = scontoMinori.toFixed(2);
+
