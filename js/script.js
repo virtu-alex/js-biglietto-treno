fb868a0d11fc797b85c914c68986f8455d6b929b
virtu-alex <alessio.cordari@gmail.com>
2022-06-21 17:55:30 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -10,21 +10,21 @@ L'output del prezzo finale va stampato in pagina forma umana (con massimo due de
 
 
 
-const firstName = prompt("Qual'e' il tuo nome?", 'Teacher')
-const nome = document.getElementById('nome-utente');
-nome.innerHTML = firstName
+// const firstName = prompt("Qual'e' il tuo nome?", 'Teacher')
+// const nome = document.getElementById('nome-utente');
+// nome.innerHTML = firstName
 
-const lastName = prompt("Qual'e' il tuo cognome?", 'Boolean')
-const cognome = document.getElementById('cognome-utente');
-cognome.innerHTML = lastName
+// const lastName = prompt("Qual'e' il tuo cognome?", 'Boolean')
+// const cognome = document.getElementById('cognome-utente');
+// cognome.innerHTML = lastName
 
-const region_a = prompt("Da dove parti? ", 'Lazio')
-const partenza = document.getElementById('region-start');
-partenza.innerHTML = region_a
+// const region_a = prompt("Da dove parti? ", 'Lazio')
+// const partenza = document.getElementById('region-start');
+// partenza.innerHTML = region_a
 
-const region_b = prompt('Dove vuoi arrivare?', "Lombardia")
-const arrivo = document.getElementById('region-arrival');
-arrivo.innerHTML = region_b
+// const region_b = prompt('Dove vuoi arrivare?', "Lombardia")
+// const arrivo = document.getElementById('region-arrival');
+// arrivo.innerHTML = region_b
 
 /*
 1- RECUPERO ELEMENTO NEL MIO FOGLIO HTML
@@ -38,39 +38,23 @@ arrivo.innerHTML = region_b
 
 // PRIMO PROMPT, KM DA PERCORRERE
 
-const kilometers = parseInt(prompt('Quanti kilometri devi percorrere?', "379,41"));
+const kilometers = parseInt(prompt('Quanti kilometri devi percorrere?', "379"));
 console.log('I kilometri da percorrere sono ' + kilometers);
 
 let prezzo = kilometers * 0.21
+
 // SECONDO PROMPT, ETA'
 
 const userAge = parseInt(prompt('Quanti anni hai?'))
 console.log("l'eta' che hai scelto e' " + userAge);
 
-// SCONTO PER I MINORI DI 18 ANNI
-
-const discountMinor = (kilometers / 100) * 20;
-console.log("lo sconto per i minorenni e' di " + discountMinor);
-
-// SCONTO PER GLI OVER 65
-
-const discountMajor = (kilometers / 100) * 40;
-console.log("lo sconto per gli over 65 e' di " + discountMajor);
-
 //PRIMA CONDIZIONE SCONTO MINORENNI
 
-if (userAge <= 18) {
-    prezzo = Math.round(prezzo - prezzo - discountMinor);
+if (userAge < 18) {
+    prezzo = (prezzo - prezzo * 0.2).toFixed(2);
     console.log('lo sconto applicato e ' + prezzo);
-} else if(userAge > 19 || userAge < 66) {
-    prezzo = 0;
-}
-
-//SECONDA CONDIZIONE SCONTO OVER 65
-
-if (userAge >= 65) {
-    prezzo = Math.round(prezzo - prezzo - discountMajor);
-    console.log('lo sconto applicato dal prezzo e' + prezzo);
+} else (userAge >= 65); {
+    prezzo = (prezzo - prezzo * 0.4).toFixed(2);
 }
 
 //STAMPO IN PAGINA
